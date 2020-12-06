3868ecdf1acd319a098c15bf3073b27371889ce8
ascel <48783351+ascel@users.noreply.github.com>
2020-12-06 20:23:50 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,11 @@
                 <div class="col-md-8">
                     <div class="card">
                         <div class="card-header">{{ __('Dashboard') }}</div>
-                            <div class="card-body">
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
+                        <div class="card-body">
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
